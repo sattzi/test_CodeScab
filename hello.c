87be000f623aadcad7fed8cc1f273b7f9475c597
sattzi <sattzi@gmail.com>
2024-02-15 16:07:38 +0900

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
-main()
+void main()
 {
   printf("Hello World\n");
+}
+int f(char * s, unsigned size) {
+	char* buf = (char*)malloc(size);
+
+	strncpy(buf, s, size + 1); // wrong: copy may exceed size of buf
+
+	for (int i = 0; i <= size; i++) { // wrong: upper limit that is higher than size of buf
+		cout << buf[i];
+	}
+  return 0;
 }
